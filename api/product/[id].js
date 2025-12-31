019eee935c16d039a8f99810f7263758981323d4
elfr3on1 <mahmoudmhmed805@gmail.com>
2025-12-31 21:17:12 +0200

    Update [id].js

--- api/product/[id].js
+++ api/product/[id].js
@@ -7,7 +7,9 @@ if (!admin.apps.length) {
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       // هذا السطر يعالج مشكلة الأسطر الجديدة في المفتاح السري
-      privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n").replace(/"/g, "")
+  : undefined,
     }),
   });
 }
